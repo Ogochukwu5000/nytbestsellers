d5b6fadd462e5ee3b97385d8cad80bf05ea2bc7f
ogochukwu5000 <jennifesther@gmail.com>
2024-03-15 12:35:43 -0500

    finished

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksFragment.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksFragment.kt
@@ -1,6 +1,7 @@
 package com.codepath.bestsellerlistapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,19 @@ import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.codepath.bestsellerlistapp.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Headers
+import org.json.JSONObject
 
 // --------------------------------//
 // CHANGE THIS TO BE YOUR API KEY  //
 // --------------------------------//
-private const val API_KEY = "<YOUR-API-KEY-HERE>"
+private const val API_KEY = "WC1H7Y3tmod9gR6TPkNAHODVSEd7EQXV"
 
 /*
  * The class for the only fragment in the app, which contains the progress bar,
@@ -44,17 +52,13 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
      */
     private fun updateAdapter(progressBar: ContentLoadingProgressBar, recyclerView: RecyclerView) {
         progressBar.show()
-
         // Create and set up an AsyncHTTPClient() here
-
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        params["api-key"] = API_KEY
         // Using the client, perform the HTTP request
-
-        /* Uncomment me once you complete the above sections!
+        client["https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json", params, object: JsonHttpResponseHandler()
         {
-            /*
-             * The onSuccess function gets called when
-             * HTTP response status is "200 OK"
-             */
             override fun onSuccess(
                 statusCode: Int,
                 headers: Headers,
@@ -62,20 +66,22 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
             ) {
                 // The wait for a response is over
                 progressBar.hide()
-
                 //TODO - Parse JSON into Models
 
-                val models : List<BestSellerBook> = null // Fix me!
-                recyclerView.adapter = BestSellerBooksRecyclerViewAdapter(models, this@BestSellerBooksFragment)
-
                 // Look for this in Logcat:
-                Log.d("BestSellerBooksFragment", "response successful")
-            }
+                val resultsJSON : JSONObject = json.jsonObject.get("results") as JSONObject
 
-            /*
-             * The onFailure function gets called when
-             * HTTP response status is "4XX" (eg. 401, 403, 404)
-             */
+                val booksRawJSON : String = resultsJSON.get("books").toString()
+
+                val gson = Gson()
+                val arrayBookType = object : TypeToken<List<BestSellerBook>>() {}.type
+                val models : List<BestSellerBook>? = gson.fromJson(booksRawJSON, arrayBookType)
+                recyclerView.adapter = models?.let { BestSellerBooksRecyclerViewAdapter(it, this@BestSellerBooksFragment)
+                }
+                Log.d("BestSellerBooksFragment", "response successful")
+//                Log.d("JSON Response", json.toString())
+
+            }
             override fun onFailure(
                 statusCode: Int,
                 headers: Headers?,
@@ -84,15 +90,12 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
             ) {
                 // The wait for a response is over
                 progressBar.hide()
-
                 // If the error is not null, log it!
                 t?.message?.let {
                     Log.e("BestSellerBooksFragment", errorResponse)
                 }
             }
         }]
-        */
-
     }
 
     /*
@@ -103,3 +106,6 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
     }
 
 }
+
+
+
